203eb0bb5056bb16173cfd190bd8f00d5f67c0ca
Gerald Plusquellec Qc <gplusquellec@gmail.com>
2014-08-05 19:13:55 +0200

    test

---
+++ Test.php
@@ -0,0 +1,11 @@
+<?php
+    namespace Thin;
+    use \PHPUnit_Framework_TestCase as TestCase;
+
+    class Test extends TestCase
+    {
+        public function setUp()
+        {
+            /* ENVIRONMENT */
+        }
+    }
