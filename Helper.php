2d7a08fd973c2d9208e74d954f403935b91ee862
Gerald Plusquellec <gplusquellec@gmail.com>
2014-12-04 07:38:22 +0100

    binary

--- Helper.php
+++ Helper.php
@@ -4576,6 +4576,7 @@ $(document).ready(function() {
             return $i;
         }
     }
+
     if (!function_exists('ThinLog')) {
         function ThinLog($message, $logFile = null, $type = 'info')
         {
@@ -4589,4 +4590,93 @@ $(document).ready(function() {
 
             File::append($logFile, date('Y-m-d H:i:s') . "\t" . Inflector::upper($type) . "\t$message\n");
         }
+    }
+
+    function binCompress($string)
+    {
+        $dictionary = array_flip(range("\0", "\xFF"));
+        $word       = '';
+        $codes      = [];
+
+        for ($i = 0; $i < strlen($string); $i++) {
+            $x = $string[$i];
+
+            if (strlen($x) && isset($dictionary[$word . $x])) {
+                $word .= $x;
+            } elseif ($i) {
+                $codes[] = $dictionary[$word];
+                $dictionary[$word . $x] = count($dictionary);
+                $word = $x;
+            }
+        }
+
+        $dictionary_count   = 256;
+        $bits               = 8;
+        $return             = '';
+        $rest               = 0;
+        $rest_length        = 0;
+
+        foreach ($codes as $code) {
+            $rest = ($rest << $bits) + $code;
+            $rest_length += $bits;
+            $dictionary_count++;
+
+            if ($dictionary_count >> $bits) {
+                $bits++;
+            }
+
+            while ($rest_length > 7) {
+                $rest_length -= 8;
+                $return .= chr($rest >> $rest_length);
+                $rest &= (1 << $rest_length) - 1;
+            }
+        }
+
+        return $return . ($rest_length ? chr($rest << (8 - $rest_length)) : "");
+    }
+
+    function binUncompress($binary)
+    {
+        $dictionary_count   = 256;
+        $bits               = 8;
+        $codes              = [];
+        $rest               = 0;
+        $rest_length        = 0;
+
+        for ($i = 0; $i < strlen($binary); $i++) {
+            $rest = ($rest << 8) + ord($binary[$i]);
+            $rest_length += 8;
+
+            if ($rest_length >= $bits) {
+                $rest_length -= $bits;
+                $codes[] = $rest >> $rest_length;
+                $rest &= (1 << $rest_length) - 1;
+                $dictionary_count++;
+
+                if ($dictionary_count >> $bits) {
+                    $bits++;
+                }
+            }
+        }
+
+        $dictionary = range("\0", "\xFF");
+        $return = '';
+
+        foreach ($codes as $i => $code) {
+            $element = $dictionary[$code];
+
+            if (!isset($element)) {
+                $element = $word . $word[0];
+            }
+
+            $return .= $element;
+
+            if ($i) {
+                $dictionary[] = $word . $element[0];
+            }
+
+            $word = $element;
+        }
+
+        return $return;
     }
