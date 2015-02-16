e4db3ec8951edde3da245be9b82f291ec1f39b2b
Gérald Plusquellec <gplusquellec@gmail.com>
2015-02-16 16:11:45 +0100

    flood

--- Flood.php
+++ Flood.php
@@ -6,42 +6,46 @@
         public function check()
         {
             /* CLI case */
-            if (!defined('APPLICATION_ENV')) {
-                define('APPLICATION_ENV', 'production');
-            }
+            defined('APPLICATION_ENV') || define('APPLICATION_ENV', 'production');
 
             if ('production' == APPLICATION_ENV) {
                 $ip = $_SERVER['REMOTE_ADDR'];
 
                 $this->isBanned($ip);
 
-                $key = $ip . '::' . date('dmYHi') . '::flood';
+                $key = 'ip.' . str_replace('.', '', $ip) . '.' . date('dmYHi') . '.flood';
 
                 $val = redis()->incr($key);
                 redis()->expire($key, 60);
 
-                if ($val > Config::get('application.flood.max.page.by.minute', 30)) {
+                if ($val > Config::get('application.flood.maxPageByMinute', 30)) {
                     $this->checkedBanned($ip);
-                    die('Flood');
+                    Api::forbidden();
                 }
             }
         }
 
         private function isBanned($ip)
         {
-            $row = jdb('system', 'banned')->where('ip = ' . str_replace('.', '_', $ip))->first(true);
+            $row = rdb('core', 'banned')
+            ->inCache(false)
+            ->where(['ip', '=', (int) str_replace('.', '', $ip)])
+            ->first(true);
 
             if ($row) {
-                die('Banned');
+                Api::forbidden();
             }
         }
 
         private function checkedBanned($ip)
         {
-            $row = jdb('system', 'flood')->where('ip = ' . str_replace('.', '_', $ip))->first(true);
+            $row = rdb('core', 'flood')
+            ->inCache(false)->where(['ip', '=', (int) str_replace('.', '', $ip)])->first(true);
 
             if (!$row) {
-                jdb('system', 'flood')->create(['ip' => str_replace('.', '_', $ip), 'num' => 1])->save();
+                rdb('core', 'flood')
+                ->create(['ip' => (int) str_replace('.', '', $ip), 'num' => 1])
+                ->save();
             } else {
                 $num = (int) $row->num;
                 $num++;
@@ -49,7 +53,9 @@
                 $row->setNum($num)->save();
 
                 if ($num >= Config::get('application.flood.max.time', 3)) {
-                    jdb('system', 'banned')->create(['ip' => str_replace('.', '_', $ip)])->save();
+                    rdb('core', 'banned')
+                    ->create(['ip' => (int) str_replace('.', '', $ip)])
+                    ->save();
                 }
             }
         }
